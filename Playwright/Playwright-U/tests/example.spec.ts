c61e01c69611706f709a49efa4d7795e3fd1a6e0
Daniel Cadena <31937742+dammahrino@users.noreply.github.com>
2022-10-20 22:37:51 -0500

    Clicking on elements, and some selectors

--- Playwright/Playwright-U/tests/example.spec.ts
+++ Playwright/Playwright-U/tests/example.spec.ts
@@ -2,6 +2,15 @@ import { test, expect } from '@playwright/test'
 
 test('Simple basic test', async ({ page }) => {
   await page.goto('https://www.example.com')
-  const pageTitle = await page.locator('h1')
+  const pageTitle = page.locator('h1')
   await expect(pageTitle).toContainText('Example Domain')
+})
+
+test('Clicking on Elements', async ({ page }) => {
+  await page.goto('http://zero.webappsecurity.com/index.html')
+  await page.click('#signin_button')
+  await page.click('text=Sign in')
+
+  const errorMessage = page.locator('.alert-error')
+  await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
