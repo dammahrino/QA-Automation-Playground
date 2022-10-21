810d3575b8ee3fb4ea2592eabaeba6bec6d78764
Daniel Cadena <31937742+dammahrino@users.noreply.github.com>
2022-10-20 22:57:32 -0500

    Discovering the most common selectors

--- Playwright/Playwright-U/tests/example.spec.ts
+++ Playwright/Playwright-U/tests/example.spec.ts
@@ -14,3 +14,23 @@ test('Clicking on Elements', async ({ page }) => {
   const errorMessage = page.locator('.alert-error')
   await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
+
+test.skip('Selectors', async ({ page }) => {
+  // Text Selectors
+  await page.click('text=some text')
+
+  // CSS Selectors (ID, Class, HTML Element)
+  await page.click('buttons')
+  await page.click('#id')
+  await page.click('.class')
+
+  // Only visible CSS Selector
+  // Playwright will only click the button if it's visible
+  await page.click('.submit-button:visible')
+
+  // Selector combinations
+  await page.click('#username .first')
+
+  // XPath
+  await page.click('//button')
+})
