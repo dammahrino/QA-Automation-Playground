12c396736121c3161ed880b8774e224b35e606b8
Daniel Cadena <31937742+dammahrino@users.noreply.github.com>
2022-10-20 23:10:59 -0500

    Basic assertions in Playwright

--- Playwright/Playwright-U/tests/example.spec.ts
+++ Playwright/Playwright-U/tests/example.spec.ts
@@ -35,10 +35,34 @@ test.skip('Selectors', async ({ page }) => {
   await page.click('//button')
 })
 
-test('Working with Inputs', async ({page}) => {
+test('Working with Inputs', async ({ page }) => {
   await page.goto('http://zero.webappsecurity.com/index.html')
   await page.click('#signin_button')
 
   await page.type('#user_login', 'some username')
   await page.type('#user_password', 'some password')
+  await page.click('text=Sign in')
+
+  const errorMessage = page.locator('.alert-error')
+  await expect(errorMessage).toContainText('Login and/or password are wrong.')
+})
+
+test('Assertions', async ({ page }) => {
+  await page.goto('https://example.com')
+  // Good for redirects
+  await expect(page).toHaveURL('https://example.com')
+
+  await expect(page).toHaveTitle('Example Domain')
+
+  const element = await page.locator('h1')
+  
+  await expect(element).toBeVisible()
+
+  await expect(element).toHaveText('Example Domain')
+
+  await expect(element).toHaveCount(1)
+
+  const nonExistingElement = await page.locator('h5')
+
+  await expect(nonExistingElement).not.toBeVisible()
 })
